3245de0472568ae3714745be657a02716bf51439
neeha-praveen <neeha.npraveen@gmail.com>
2025-07-20 21:33:49 +0530

    Complete Task 4

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -52,8 +52,12 @@ public_users.get('/author/:author',function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title;
+  keys.forEach((key)=>{
+    if(books[key].title===title){
+      res.json({book:books[key]});
+    }
+  })
 });
 
 //  Get book review
